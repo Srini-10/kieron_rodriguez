f061d68780f2ba06660fb416c7f7b3f9f4ab7e55
Srini-10 <srinivasanp1018@gmail.com>
2024-11-26 21:38:47 +0530

    changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
     <>
       <div className="w-full bg-orange-50 flex flex-col justify-center items-center">
         <div className="w-full -mt-[2px] bg-orange-50 flex justify-center items-center">
-          <img src={Head} className="w-full unselectable" alt="Ring" />
+          <img src={Head} className="w-full unselectable" alt="" />
           <div className="absolute w-full h-[1534px] z-10"></div>
         </div>
       </div>
